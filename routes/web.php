c474aec44738c7fb6c3a819e68aa19fd5c2a5c82
MasKanz <kanzalkautsar@gmail.com>
2025-04-25 21:34:05 +0700

    Change of plans

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/home', [HomeController::class, 'index']);
 Route::get('/shop', [ProductPageController::class, 'index']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware(LoginUserCheck::class);
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 Route::get('/contacts', [ContactController::class, 'index']);
